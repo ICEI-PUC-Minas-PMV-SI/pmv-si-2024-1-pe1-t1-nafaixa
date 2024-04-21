0a87a030171ebcfb463859d21a55540608ecd52e
Rita laura <ritalaura57@gmail.com>
2024-04-21 12:56:09 -0300

    Identificação individual dos eventos e melhorias de código #46

--- src/JS/mapa.js
+++ src/JS/mapa.js
@@ -44,171 +44,95 @@ function buildContent(property) {
   alt="event img"/>
   </div>
 
-<div class="details">
-  <p class="event-title">Nome do evento</p>
-  <p><img src="./assets/img/data.svg" alt="Data do evento" />Data</p>
-  <p>
-    <img src="./assets/img/local.svg" alt="Local do evento" />Local
-  </p>
-</div>
+  <div class="details">
+    <p class="event-title">${property.nome}</p>
+    <p><img src="./assets/img/data.svg" alt="Data do evento" />Data</p>
+    <p>
+      <img src="./assets/img/local.svg" alt="Local do evento" />${property.endereco}
+    </p>
+  </div>
 
   `;
-  // `
-  // <div class="icon">
-  //     <i aria-hidden="true" class="fa fa-icon fa-${property.type}" title="${property.type}"></i>
-  //     <span class="fa-sr-only">${property.type}</span>
-  // </div>
-  // <div class="details">
-  //     <div class="price">${property.price}</div>
-  //     <div class="address">${property.address}</div>
-  //     <div class="features">
-  //     <div>
-  //         <i aria-hidden="true" class="fa fa-bed fa-lg bed" title="bedroom"></i>
-  //         <span class="fa-sr-only">bedroom</span>
-  //         <span>${property.bed}</span>
-  //     </div>
-  //     <div>
-  //         <i aria-hidden="true" class="fa fa-bath fa-lg bath" title="bathroom"></i>
-  //         <span class="fa-sr-only">bathroom</span>
-  //         <span>${property.bath}</span>
-  //     </div>
-  //     <div>
-  //         <i aria-hidden="true" class="fa fa-ruler fa-lg size" title="size"></i>
-  //         <span class="fa-sr-only">size</span>
-  //         <span>${property.size} ft<sup>2</sup></span>
-  //     </div>
-  //     </div>
-  // </div>
-  // `;
+
   return content;
 }
 
 const properties = [
   {
-    address: "215 Emily St, MountainView, CA",
-    description: "Single family house with modern design",
-    price: "$ 3,889,000",
-    type: "home",
-    bed: 5,
-    bath: 4.5,
-    size: 300,
+    nome: "Evento1",
+    endereco: "215 Emily St, MountainView, CA",
     position: {
       lat: 37.50024109655184,
       lng: -122.28528451834352,
     },
   },
   {
-    address: "108 Squirrel Ln &#128063;, Menlo Park, CA",
-    description: "Townhouse with friendly neighbors",
-    price: "$ 3,050,000",
-    type: "building",
-    bed: 4,
-    bath: 3,
-    size: 200,
+    nome: "Evento2",
+    endereco: "108 Squirrel Ln &#128063;, Menlo Park, CA",
     position: {
       lat: 37.44440882321596,
       lng: -122.2160620727,
     },
   },
   {
-    address: "100 Chris St, Portola Valley, CA",
-    description: "Spacious warehouse great for small business",
-    price: "$ 3,125,000",
-    type: "warehouse",
-    bed: 4,
-    bath: 4,
-    size: 800,
+    nome: "Evento3",
+    endereco: "100 Chris St, Portola Valley, CA",
     position: {
       lat: 37.39561833718522,
       lng: -122.21855116258479,
     },
   },
   {
-    address: "98 Aleh Ave, Palo Alto, CA",
-    description: "A lovely store on busy road",
-    price: "$ 4,225,000",
-    type: "store-alt",
-    bed: 2,
-    bath: 1,
-    size: 210,
+    nome: "Evento4",
+    endereco: "98 Aleh Ave, Palo Alto, CA",
     position: {
       lat: 37.423928529779644,
       lng: -122.1087629822001,
     },
   },
   {
-    address: "2117 Su St, MountainView, CA",
-    description: "Single family house near golf club",
-    price: "$ 1,700,000",
-    type: "home",
-    bed: 4,
-    bath: 3,
-    size: 200,
+    nome: "Evento5",
+    endereco: "2117 Su St, MountainView, CA",
     position: {
       lat: 37.40578635332598,
       lng: -122.15043378466069,
     },
   },
   {
-    address: "197 Alicia Dr, Santa Clara, CA",
-    description: "Multifloor large warehouse",
-    price: "$ 5,000,000",
-    type: "warehouse",
-    bed: 5,
-    bath: 4,
-    size: 700,
+    nome: "Evento6",
+    endereco: "197 Alicia Dr, Santa Clara, CA",
     position: {
       lat: 37.36399747905774,
       lng: -122.10465384268522,
     },
   },
   {
-    address: "700 Jose Ave, Sunnyvale, CA",
-    description: "3 storey townhouse with 2 car garage",
-    price: "$ 3,850,000",
-    type: "building",
-    bed: 4,
-    bath: 4,
-    size: 600,
+    nome: "Evento7",
+    endereco: "700 Jose Ave, Sunnyvale, CA",
     position: {
       lat: 37.38343706184458,
       lng: -122.02340436985183,
     },
   },
   {
-    address: "868 Will Ct, Cupertino, CA",
-    description: "Single family house in great school zone",
-    price: "$ 2,500,000",
-    type: "home",
-    bed: 3,
-    bath: 2,
-    size: 100,
+    nome: "Evento8",
+    endereco: "868 Will Ct, Cupertino, CA",
     position: {
       lat: 37.34576403052,
       lng: -122.04455090047453,
     },
   },
   {
-    address: "655 Haylee St, Santa Clara, CA",
-    description: "2 storey store with large storage room",
-    price: "$ 2,500,000",
-    type: "store-alt",
-    bed: 3,
-    bath: 2,
-    size: 450,
+    nome: "Evento9",
+    endereco: "655 Haylee St, Santa Clara, CA",
     position: {
       lat: 37.362863347890716,
       lng: -121.97802139023555,
     },
   },
   {
-    address: "2019 Natasha Dr, San Jose, CA",
-    description: "Single family house",
-    price: "$ 2,325,000",
-    type: "home",
-    bed: 4,
-    bath: 3.5,
-    size: 500,
+    nome: "Evento10",
+    endereco: "2019 Natasha Dr, San Jose, CA",
     position: {
       lat: 37.41391636421949,
       lng: -121.94592071575907,
