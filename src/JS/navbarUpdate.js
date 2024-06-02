52eb5e2d213412d42d7efa4075814f6c267d1428
amdmags <amdmagsdev@gmail.com>
2024-06-02 15:55:33 -0300

    fixed funtion

--- src/JS/navbarUpdate.js
+++ src/JS/navbarUpdate.js
@@ -11,24 +11,17 @@ document.addEventListener('DOMContentLoaded', function () {
         const navbarLoggedOutMobile = document.getElementById('navbar-logged-out-mobile');
         const navbarLoggedIn = document.getElementById('navbar-logged-in');
 
-        try {
-            const parsedUserData = JSON.parse(userData);
-
-            if (parsedUserData && parsedUserData.id) {
-                navbarLoggedOutMobile.style.display = 'none';
-                navbarLoggedOut.style.display = 'none';
-                navbarLoggedIn.style.display = 'block';
-            } else {
-                navbarLoggedOutMobile.style.display = 'block';
-                navbarLoggedOut.style.display = 'block';
-                navbarLoggedIn.style.display = 'none';
-            }
-        } catch (error) {
+        if (userData && userData.length > 0) {
+            navbarLoggedOutMobile.style.display = 'none';
+            navbarLoggedOut.style.display = 'none';
+            navbarLoggedIn.style.display = 'block';
+        } else {
             navbarLoggedOutMobile.style.display = 'block';
             navbarLoggedOut.style.display = 'block';
             navbarLoggedIn.style.display = 'none';
         }
     }
+
 
     updateNavbar();
 });
