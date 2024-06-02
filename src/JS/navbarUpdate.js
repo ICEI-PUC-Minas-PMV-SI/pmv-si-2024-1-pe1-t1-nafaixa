15ebdaefbae7b26d6bfc63912a143cd36fc50125
amdmags <amdmagsdev@gmail.com>
2024-06-02 17:28:07 -0300

    navbar shows producer pic and hide searchform. Need to fix css bugs

--- src/JS/navbarUpdate.js
+++ src/JS/navbarUpdate.js
@@ -7,21 +7,54 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function updateNavbar() {
         const userData = localStorage.getItem('userData');
-        const navbarLoggedOut = document.getElementById('navbar-logged-out');
         const navbarLoggedOutMobile = document.getElementById('navbar-logged-out-mobile');
+        const navbarLoggedOut = document.getElementById('navbar-logged-out');
         const navbarLoggedIn = document.getElementById('navbar-logged-in');
+        const searchBars = document.querySelectorAll('.nav-search');
 
-        if (userData && userData.length > 0) {
-            navbarLoggedOutMobile.style.display = 'none';
-            navbarLoggedOut.style.display = 'none';
-            navbarLoggedIn.style.display = 'block';
+        navbarLoggedOutMobile.style.display = userData ? 'none' : 'flex';
+        navbarLoggedOut.style.display = userData ? 'none' : 'flex';
+        navbarLoggedIn.style.display = userData ? 'flex' : 'none';
+
+        if (window.innerWidth < 864 && userData) {
+            searchBars.forEach(searchBar => {
+                searchBar.style.display = 'none';
+            });
         } else {
-            navbarLoggedOutMobile.style.display = 'block';
-            navbarLoggedOut.style.display = 'block';
-            navbarLoggedIn.style.display = 'none';
+            searchBars.forEach(searchBar => {
+                searchBar.style.display = 'flex';
+            });
         }
     }
 
+    window.addEventListener('resize', updateNavbar);
 
-    updateNavbar();
+    document.addEventListener('DOMContentLoaded', updateNavbar);
+
+    async function updateProdutorImage() {
+        const produtorId = localStorage.getItem('userData');
+        if (produtorId) {
+            const produtorImg = document.getElementById('logo-produtor-menu');
+            try {
+                const response = await fetch(`http://localhost:3000/produtor/${produtorId}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    if (data.logo) {
+                        produtorImg.src = data.logo;
+                    } else {
+                        console.error('URL da imagem do produtor está vazia.');
+                    }
+                } else {
+                    console.error('Erro ao obter dados do produtor:', response.status);
+                }
+            } catch (error) {
+                console.error('Erro ao buscar dados do produtor:', error);
+            }
+        }
+    }
+
+    window.onload = function () {
+        updateNavbar();
+        updateProdutorImage();
+    };
 });
