62082ff6a9985cad7844560579b91773d6cfc315
amdmags <amdmagsdev@gmail.com>
2024-06-02 15:47:42 -0300

    navbar changing. need to fix css

--- src/JS/navbarUpdate.js
+++ src/JS/navbarUpdate.js
@@ -1,16 +1,34 @@
-function updateNavbar() {
-    const userData = JSON.parse(localStorage.getItem('userData'));
-    const navbarLoggedOut = document.getElementsById('navbar-logged-out');
-    const navbarLoggedIn = document.getElementById('navbar-logged-in');
-  
-    if (userData && userData.id) {
-        navbarLoggedOut.style.display = 'none';
-        navbarLoggedIn.style.display = 'block';
-    } else {
-        navbarLoggedOut.style.display = 'block';
-        navbarLoggedIn.style.display = 'none';
+document.addEventListener('DOMContentLoaded', function () {
+    const meuPerfilLink = document.getElementById('meu-perfil');
+    meuPerfilLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        window.location.href = 'cadastro-produtor.html?edit';
+    });
+
+    function updateNavbar() {
+        const userData = localStorage.getItem('userData');
+        const navbarLoggedOut = document.getElementById('navbar-logged-out');
+        const navbarLoggedOutMobile = document.getElementById('navbar-logged-out-mobile');
+        const navbarLoggedIn = document.getElementById('navbar-logged-in');
+
+        try {
+            const parsedUserData = JSON.parse(userData);
+
+            if (parsedUserData && parsedUserData.id) {
+                navbarLoggedOutMobile.style.display = 'none';
+                navbarLoggedOut.style.display = 'none';
+                navbarLoggedIn.style.display = 'block';
+            } else {
+                navbarLoggedOutMobile.style.display = 'block';
+                navbarLoggedOut.style.display = 'block';
+                navbarLoggedIn.style.display = 'none';
+            }
+        } catch (error) {
+            navbarLoggedOutMobile.style.display = 'block';
+            navbarLoggedOut.style.display = 'block';
+            navbarLoggedIn.style.display = 'none';
+        }
     }
-  }
-  
-  
-  updateNavbar();
+
+    updateNavbar();
+});
