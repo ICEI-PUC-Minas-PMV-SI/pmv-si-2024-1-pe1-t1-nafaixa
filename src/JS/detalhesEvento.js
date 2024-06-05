44c44acadcea68f243b46f65392ccb06def7c75d
amdmags <amdmagsdev@gmail.com>
2024-06-05 19:40:36 -0300

    fixed bug - now email is saving email if user is interested in a event

--- src/JS/detalhesEvento.js
+++ src/JS/detalhesEvento.js
@@ -1,94 +1,3 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const idEvento = urlParams.get('id');
-
-    if (!idEvento) {
-        console.error('ID do evento não encontrado na URL.');
-        return;
-    }
-
-    try {
-        const apiUrlEvento = `http://localhost:3000/eventos/${idEvento}`;
-        const responseEvento = await fetch(apiUrlEvento);
-        const evento = await responseEvento.json();
-
-        const imageUrl = evento.bannerURL;
-        document.querySelector('.banner-evento').style.backgroundImage = `url('${imageUrl}')`;
-        document.querySelector('.imagem-blur').style.backgroundImage = `url('${imageUrl}')`;
-
-        document.querySelector('.nome-evento').textContent = evento.nome;
-        document.getElementById('categoria-evento').innerHTML = `<img src="assets/img/tag.svg" alt="categoria" /> ${evento.categoria}`;
-        document.getElementById('data-inicio-evento').innerHTML = `<img src="./assets/img/data.svg" alt="Data do evento"/> ${formatarDataHora(evento.startDate)}`;
-        document.getElementById('hora-inicio-evento').textContent = formatarHora(evento.startTime);
-        document.getElementById('data-final-evento').innerHTML = `${formatarDataHora(evento.finalDate)}`;
-        document.getElementById('hora-final-evento').textContent = formatarHora(evento.finalTime);
-
-        const localElement = document.getElementById('localEvento');
-        localElement.innerHTML = evento.tipo === 'presencial' ?
-            `<img src="./assets/img/local.svg" alt="Local do evento" /> ${evento.local}` :
-            `<img src="./assets/img/local.svg" alt="Local do evento" /> ${evento.link}`;
-
-        document.getElementById('descricao-evento').innerHTML = evento.sobre;
-        document.getElementById('informacoes-evento').textContent = evento.informacoes;
-
-        const produtorId = evento.produtorId;
-        if (produtorId) {
-            const apiUrlProdutor = `http://localhost:3000/produtor/${produtorId}`;
-            const responseProdutor = await fetch(apiUrlProdutor);
-            const produtor = await responseProdutor.json();
-            document.getElementById('sobre-produtor').textContent = produtor.descricao;
-
-            const logoProdutorElement = document.querySelector('.sobre-produtor-icone');
-            logoProdutorElement.src = produtor.logo;
-            logoProdutorElement.alt = `Logo de ${produtor.nome}`;
-        } else {
-            console.error('ID do produtor não encontrado no evento.');
-        }
-    } catch (error) {
-        console.error('Erro ao carregar detalhes do evento:', error);
-    }
-});
-
-
-function formatarDataHora(data) {
-    const partes = data.split('/');
-
-    if (partes.length !== 3) {
-        console.error('Formato de data inválido:', data);
-        return null;
-    }
-
-    const dia = parseInt(partes[0], 10);
-    const mesNumero = parseInt(partes[1], 10);
-    const ano = parseInt(partes[2], 10);
-
-    const meses = [
-        'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
-        'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
-    ];
-
-    const mesNome = meses[mesNumero - 1];
-
-    return `${dia} de ${mesNome} de ${ano}`;
-}
-
-function formatarHora(hora) {
-    const partes = hora.split(':');
-
-    if (partes.length !== 2) {
-        console.error('Formato de hora inválido:', hora);
-        return null;
-    }
-
-    const horas = parseInt(partes[0], 10);
-    const minutos = parseInt(partes[1], 10);
-    return `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}`;
-}
-
-//------------------------------------------------------------------------------------------//
-
-//Função para capturar o email e salvar no banco de dados
-
 document.addEventListener('DOMContentLoaded', () => {
     const enviarEmailBtn = document.querySelector('.enviar-email-btn');
 
@@ -97,15 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const emailInput = document.getElementById('input-email').value;
 
-        if (emailInput.trim() === '') {
+        if (!isValidEmail(emailInput)) {
             alert('Por favor, insira um endereço de e-mail válido.');
             return;
         }
 
-        const idEvento = JSON.parse(localStorage.getItem('eventoDetalhes')).id;
+        const idEvento = getParameterByName('id');
 
         if (!idEvento) {
-            alert('ID do evento não encontrado no localStorage.');
+            alert('ID do evento não encontrado na URL.');
             return;
         }
 
@@ -115,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-            })
+            });
 
             if (!resp.ok) {
                 throw new Error('Falha ao verificar email. Tente novamente mais tarde.');
@@ -124,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const emailFound = await resp.json();
 
             if (emailFound.length > 0) {
-                throw new Error('Email ja cadastrado para esse evento.')
+                throw new Error('Email já cadastrado para esse evento.');
             }
 
             const response = await fetch('http://localhost:3000/emails', {
@@ -150,3 +59,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+function getParameterByName(name) {
+    const url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+    const results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+// Função para validar email
+function isValidEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
