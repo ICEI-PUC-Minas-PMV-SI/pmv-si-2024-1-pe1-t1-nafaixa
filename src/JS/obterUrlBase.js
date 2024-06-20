bce117443c0f8b802a9ef7bad39c69e34cacb361
Rita laura <ritalaura57@gmail.com>
2024-06-20 14:23:24 -0300

    configurando para rodar localmente

--- src/JS/obterUrlBase.js
+++ src/JS/obterUrlBase.js
@@ -3,7 +3,7 @@ function obterUrlBase() {
     window.location.hostname == "localhost" ||
     window.location.hostname == "127.0.0.1";
   const baseUrl = estaLocalmente
-    ? ""
+    ? "http://localhost:3000"
     : "https://na-faixa-backend-ba6af9478806.herokuapp.com";
   return baseUrl;
 }
